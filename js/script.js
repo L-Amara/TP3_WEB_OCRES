6f38ac0bbc3f75b084c961c364449cde79b05e49
L-Amara <57790461+L-Amara@users.noreply.github.com>
2021-10-03 23:01:53 +0200

    Bonus : boucle

--- js/script.js
+++ js/script.js
@@ -48,43 +48,57 @@ function start() {
 
             // ==================== Version dédoublée :
 
-            // Jour 1
+            /* // Jour 1
             // On récupère l'information principale
-            const main1 = data.list[0].weather[0].main;
-            const description1 = data.list[0].weather[0].description;
-            const temp1 = data.list[0].temp.day;
-            const icon1 = apiWeather.getHTMLElementFromIcon(data.list[0].weather[0].icon);
+            const main0 = data.list[0].weather[0].main;
+            const description0 = data.list[0].weather[0].description;
+            const temp0 = data.list[0].temp.day;
+            const icon0 = apiWeather.getHTMLElementFromIcon(data.list[0].weather[0].icon);
+            // Modifier le DOM
+            document.getElementById('today-forecast-main0').innerHTML = main0;
+            document.getElementById('today-forecast-more-info0').innerHTML = description0;
+            document.getElementById('icon-weather-container0').innerHTML = icon0;
+            document.getElementById('today-forecast-temp0').innerHTML = `${temp0}°C`;
+
+            // Jour 2
+            // On récupère l'information principale
+            const main1 = data.list[1].weather[0].main;
+            const description1 = data.list[1].weather[0].description;
+            const temp1 = data.list[1].temp.day;
+            const icon1 = apiWeather.getHTMLElementFromIcon(data.list[1].weather[0].icon);
             // Modifier le DOM
             document.getElementById('today-forecast-main1').innerHTML = main1;
             document.getElementById('today-forecast-more-info1').innerHTML = description1;
             document.getElementById('icon-weather-container1').innerHTML = icon1;
             document.getElementById('today-forecast-temp1').innerHTML = `${temp1}°C`;
 
-            // Jour 2
+            // Jour 3
             // On récupère l'information principale
-            const main2 = data.list[1].weather[0].main;
-            const description2 = data.list[1].weather[0].description;
-            const temp2 = data.list[1].temp.day;
-            const icon2 = apiWeather.getHTMLElementFromIcon(data.list[1].weather[0].icon);
+            const main2 = data.list[2].weather[0].main;
+            const description2 = data.list[2].weather[0].description;
+            const temp2 = data.list[2].temp.day;
+            const icon2 = apiWeather.getHTMLElementFromIcon(data.list[2].weather[0].icon);
             // Modifier le DOM
             document.getElementById('today-forecast-main2').innerHTML = main2;
             document.getElementById('today-forecast-more-info2').innerHTML = description2;
             document.getElementById('icon-weather-container2').innerHTML = icon2;
-            document.getElementById('today-forecast-temp2').innerHTML = `${temp2}°C`;
-
-            // Jour 3
-            // On récupère l'information principale
-            const main3 = data.list[2].weather[0].main;
-            const description3 = data.list[2].weather[0].description;
-            const temp3 = data.list[2].temp.day;
-            const icon3 = apiWeather.getHTMLElementFromIcon(data.list[2].weather[0].icon);
-            // Modifier le DOM
-            document.getElementById('today-forecast-main3').innerHTML = main3;
-            document.getElementById('today-forecast-more-info3').innerHTML = description3;
-            document.getElementById('icon-weather-container3').innerHTML = icon3;
-            document.getElementById('today-forecast-temp3').innerHTML = `${temp3}°C`;
+            document.getElementById('today-forecast-temp2').innerHTML = `${temp2}°C`; */
 
 
+            // ==================== Version avec boucle du jour 1 à jour 3
+
+            for (let i = 0; i < 3; i++) {
+                const main = data.list[i].weather[0].main;
+                const description = data.list[i].weather[0].description;
+                const temp = data.list[i].temp.day;
+                const icon = apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
+
+                // Modifier le DOM
+                document.getElementById(`today-forecast-main${i}`).innerHTML = main;
+                document.getElementById(`today-forecast-more-info${i}`).innerHTML = description;
+                document.getElementById(`icon-weather-container${i}`).innerHTML = icon;
+                document.getElementById(`today-forecast-temp${i}`).innerHTML = `${temp}°C`;
+            }
 
 
         })
