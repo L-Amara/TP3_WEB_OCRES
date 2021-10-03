b542c4311bb0cc56e5fadc5dfbfd934fdfa5476e
L-Amara <57790461+L-Amara@users.noreply.github.com>
2021-10-03 22:56:04 +0200

    Etape 4

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -7,26 +7,35 @@ const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
 
+const API_16 = "https://api.openweathermap.org/data/2.5/forecast/daily";
 
-class API_WEATHER{
-  constructor(city){
-    // Si la ville n'est pas définit alors la ville par défault est Paris
-    if(city === undefined){
-      city = "paris";
+class API_WEATHER {
+    constructor(city) {
+        // Si la ville n'est pas définit alors la ville par défault est Paris
+        if (city === undefined) {
+            city = "paris";
+        }
+        this.city = city;
     }
-    this.city = city;
-  }
 
-  // Faire la requete à l'API openweathermap
-  // Retourne une promise
-  fetchTodayForecast(){
-    return axios
-    .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
-      crossdomain: true
-    })
-  }
-  // Retourne l'element HTML de l'icon symbolisant la méteo.
-  getHTMLElementFromIcon(icon){
-    return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
-  }
+    // Faire la requete à l'API openweathermap
+    // Retourne une promise
+    fetchTodayForecast() {
+            return axios
+                .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
+                    crossdomain: true
+                })
+        }
+        // Retourne l'element HTML de l'icon symbolisant la méteo.
+    getHTMLElementFromIcon(icon) {
+        return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
+    }
+
+    getThreeDayForecast() {
+        return axios
+            .get(`${API_16}?q=${this.city}&units=metric&appid=${API_KEY}`, {
+                crossdomain: true
+            })
+
+    }
 }
